c50276f2cbd459b0f5077087df9bacdff57319a3
Amanuel Yohannes <eritreaTel@gmail.com>
2016-11-09 10:45:09 +0300

    modification of about page

--- app/pages/AboutUsPage.js
+++ app/pages/AboutUsPage.js
@@ -1,69 +1,68 @@
 const React = require('react');
 const PageTitle = require('../components/shared/pageTitle');
 
-class AboutUsPage extends React.Component {
 
-	renderAboutUgandaGuestHouses() {
-		return (
-			<div className="mg-about-features">
-				<div className="container">
-					<div className="row">
-						<div className="col-md-6">
-							<div className="mg-about-us-txt">
-								<div className="mg-sec-title">
-									<h2>About Uganda GuestHouses</h2>
-									<p>Find great apartments, literally home away from home</p>
-								</div>
-								<p>GuestHouses Uganda top mission is to give visibility to many great guest houses in Uganda to the outside world. By doing so, we will promote tourism in Uganda by making  it easy to reserve guest house in Uganda. One of the first things a tourist does when traveling to a country is finding accommodation. We want to make this process as pleasant as possible while making sure the tourist gets the best deal on the market. We want tourists to get the best out of their stay. Guesthouses are less boring than hotel and it has home feeling. TODO-Amanuel, make this good.</p>
+const AboutUgandaGuestHouse = function (props) {
+	return (
+		<div className="mg-about-features">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-6">
+						<div className="mg-about-us-txt">
+							<div className="mg-sec-title">
+								<h2>About Uganda GuestHouses</h2>
+								<p>Find great apartments, literally home away from home</p>
 							</div>
+							<p>GuestHouses Uganda top mission is to give visibility to many great guest houses in Uganda to the outside world. By doing so, we will promote tourism in Uganda by making  it easy to reserve guest house in Uganda. One of the first things a tourist does when traveling to a country is finding accommodation. We want to make this process as pleasant as possible while making sure the tourist gets the best deal on the market. We want tourists to get the best out of their stay. Guesthouses are less boring than hotel and it has home feeling. TODO-Amanuel, make this good.</p>
 						</div>
-						<div className="col-md-6">
-							<div className="video-responsive">
-								<iframe src="https://player.vimeo.com/video/134008155" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-							</div>
+					</div>
+					<div className="col-md-6">
+						<div className="video-responsive">
+							<iframe src="https://player.vimeo.com/video/134008155" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
+}
 
-	renderTestimonials(testimonials) {
-		const styledTestomonials = testimonials.map(testimonial => {
+const Testomonials = function (props) {
+	const styledTestomonials = props.testimonials.map(testimonial => {
 			return <blockquote>
 						<p>{testimonial.body}</p>
 						<footer>{testimonial.full_name}, {testimonial.title}</footer>
 					</blockquote>
-		});
+});
 
-		return(
-			<div className="mg-about-testimonial parallax">
-				<div className="container">
-					<div className="row">
-						<div className="col-md-12">
-							<div className="mg-sec-title">
-								<h2>Testimonials</h2>
-								<p>Here is some valuable word from our clients</p>
-							</div>
-							<div className="mg-testimonial-slider" id="mg-testimonial-slider">
-								{styledTestomonials}
-							</div>
+	return(
+		<div className="mg-about-testimonial parallax">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						<div className="mg-sec-title">
+							<h2>Testimonials</h2>
+							<p>Here is some valuable word from our clients</p>
+						</div>
+						<div className="mg-testimonial-slider" id="mg-testimonial-slider">
+							{styledTestomonials}
 						</div>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
+}
 
-	renderOurStaff(teamMembers) {
-		const styledTeams =teamMembers.map(member => {
-			return 	<div className="col-md-3 col-sm-6">
+const OurStaff = function (props) {
+	const styledTeams =props.teamMembers.map(member => {
+			return  <div className="col-md-3 col-sm-6">
 						<div className="mg-team-member">
 							<figure>
 								<img src={member.photo} alt="" className="img-responsive"/>
 							</figure>
 							<div className="mg-team-member-overlayer"></div>
-								<div className="mg-team-info">
+							<div className="mg-team-info">
 								<h3>{member.full_name}</h3>
 								<strong>{member.title}</strong>
 								<ul className="mg-team-member-social">
@@ -74,9 +73,9 @@ class AboutUsPage extends React.Component {
 							</div>
 						</div>
 					</div>
-		});
+			});
 
-		return (
+	return (
 			<div className="mg-team pb70">
 				<div className="container">
 					<div className="row">
@@ -92,14 +91,15 @@ class AboutUsPage extends React.Component {
 					</div>
 				</div>
 			</div>
-		);
-	}
+	);
+}
 
-	renderOurClients(partners) {
-		const styledLis = partners.map(apt => {
+const OurClients = function (props) {
+	const styledLis = props.partners.map(apt => {
 			return  <li><img src={apt.full} alt={apt.caption}/></li>
-		});
-		return (
+	});
+
+	return (
 			<div className="mg-about-clients">
 				<div className="container">
 					<div className="row">
@@ -111,17 +111,21 @@ class AboutUsPage extends React.Component {
 					</div>
 				</div>
 			</div>
-		);
-	}
+	);
+}
+
+
+
+class AboutUsPage extends React.Component {
 
 	render() {
 		return (
 			<div>
 				{<PageTitle parentClassName="mg-page-title-space parallax"/>}
-				{this.renderAboutUgandaGuestHouses()}
-				{this.renderTestimonials(this.props.testimonials)}
-				{this.renderOurStaff(this.props.teamMembers)}
-				{this.renderOurClients(this.props.partners)}
+				<AboutUgandaGuestHouse />
+				<Testomonials  testimonials = {this.props.testimonials} />
+				<OurStaff teamMembers = {this.props.teamMembers} />
+				<OurClients partners={this.props.partners} />
 			</div>
 		);
 	}
