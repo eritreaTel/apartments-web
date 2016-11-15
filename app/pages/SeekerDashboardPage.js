6516fe4babda25635cc8e3f4aac8b13a6a38af0f
Amanuel Yohannes <eritreaTel@gmail.com>
2016-11-15 16:14:25 +0300

    seeker dashboard

--- app/pages/SeekerDashboardPage.js
+++ app/pages/SeekerDashboardPage.js
@@ -1,11 +1,40 @@
 const React = require('react');
+const PageTitle = require('../components/shared/pageTitle');
+
+
+const MyReservations = function () {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-md-1"> </div>
+				<div className="col-md-10"> <h5> My Reservations </h5> </div>
+				<div className="col-md-1"> </div>
+			</div>
+		</div>
+	);
+}
+
+const MySearchs = function () {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-md-1"> </div>
+				<div className="col-md-10"> <h5> My Searches </h5> </div>
+				<div className="col-md-1"> </div>
+			</div>
+		</div>
+	);
+}
+
 
 class SeekerDashboardPage extends React.Component {
 
 	render() {
 		return (
 			<div>
-				This is guest house seeker dashboard
+				<PageTitle parentClassName="mg-page-title-space parallax"/>
+				<MyReservations />
+				<MySearchs />
 			</div>
 		);
 	}
