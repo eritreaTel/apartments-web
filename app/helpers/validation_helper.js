5e183b7ac49fe2e5311f2700f3e7cf8b83d40618
Amanuel Yohannes <eritreaTel@gmail.com>
2016-11-23 15:35:56 +0300

    addition of validator package

--- app/helpers/validation_helper.js
+++ app/helpers/validation_helper.js
@@ -1,7 +1,20 @@
 const React = require('react');
+const validator = require('validator');
 module.exports = {
     isRequired(value) {
-        return value.toString().trim().length > 0;
+        return validator.isLength(value, { min: 1 });
+    },
+
+    isEmail(value) {
+        return validator.isEmail(value);
+    },
+
+    isCreditCard(value) {
+        return validator.isCreditCard(value);
+    },
+
+    isNumeric(value) {
+        return validator.isNumeric(value);
     }
 }
 
--- app/pages/SignInPage.js
+++ app/pages/SignInPage.js
@@ -6,8 +6,9 @@ const Actions = require('../actions/actions');
 const ValidationHelper = require('../helpers/validation_helper');
 const Danger = require('../components/ribbons/danger');
 const ReactValiation = require('react-validate');
-const Validate     = ReactValiation.Validate;
-const ErrorMessage = ReactValiation.ErrorMessage;
+const Validate     	= ReactValiation.Validate;
+const ErrorMessage 	= ReactValiation.ErrorMessage;
+const ValidateGroup = ReactValiation.ValidateGroup;
 
 
 const authenticateUser = function (e) {
@@ -30,64 +31,66 @@ class SignInBody extends React.Component {
 		let errorClass = (errorMessage == null || errorMessage == undefined) ? ' col-md-6 hide' : 'col-md-6 show';
 		return (
 			<div className="mg-about-features">
-				<div className="container">
-					<div className="row">
-						<div className="col-md-3"> </div>
-						<div className={errorClass}>
-							<Danger message= {errorMessage} />
-						</div>
-						<div className="col-md-3"> </div>
-					</div>
-					<div className="row">
-						<div className="col-md-4"> </div>
-						<div className="col-md-3">
-							<div className="mg-book-form-input">
-								<label>Email Address</label><span className='required-input'> * </span>
-								<Validate validators={[ValidationHelper.isRequired]}>
-									<input tabIndex="1" ref='email' type="text" className="input-with-validation form-control" />
-								</Validate>
+				<ValidateGroup>
+					<div className="container">
+						<div className="row">
+							<div className="col-md-3"> </div>
+							<div className={errorClass}>
+								<Danger message= {errorMessage} />
 							</div>
+							<div className="col-md-3"> </div>
 						</div>
-						<div className="col-md-4"> </div>
-					</div>
-
-					<div className="row">
-						<div className="col-md-4"> </div>
-						<div className="col-md-3">
-							<div className="mg-book-form-input">
-								<label>Password</label><span className='required-input'> * </span>
-								<Validate validators={[ValidationHelper.isRequired]}>
-									<input tabIndex="2"  ref='password' type="password" className="input-with-validation form-control"/>
-								</Validate>
-							</div>
-						</div>
-						<div className="col-md-4"> </div>
-					</div>
-
-					<div className="row margin-bottom-20">
-						<div className="col-md-4"> </div>
-						<div className="col-md-3">
-							<div className="row">
-								<div className="col-md-6"> </div>
-								<div className="col-md-6">
-									<Anchor tabIndex="3" onClick = {() => {Actions.setRoute('/reset-password')}} className="margin-left-6 primary-blue">forgot password</Anchor>
+						<div className="row">
+							<div className="col-md-4"> </div>
+							<div className="col-md-3">
+								<div className="mg-book-form-input">
+									<label>Email Address</label><span className='required-input'> * </span>
+									<Validate validators={[ValidationHelper.isRequired, ValidationHelper.isEmail]}>
+										<input tabIndex="1" ref='email' type="text" className="input-with-validation form-control" />
+									</Validate>
 								</div>
 							</div>
+							<div className="col-md-4"> </div>
 						</div>
-						<div className="col-md-4"> </div>
-					</div>
 
-
-					<div className="row">
-						<div className="col-md-4"> </div>
-						<div className="col-md-3">
-							<div className="mg-book-form-input">
-								<Anchor tabIndex="4" onClick = {() => {authenticateUser(this)}} className="width-265 btn btn-primary">Sign In</Anchor>
+						<div className="row">
+							<div className="col-md-4"> </div>
+							<div className="col-md-3">
+								<div className="mg-book-form-input">
+									<label>Password</label><span className='required-input'> * </span>
+									<Validate validators={[ValidationHelper.isRequired]}>
+										<input tabIndex="2"  ref='password' type="password" className="input-with-validation form-control"/>
+									</Validate>
+								</div>
 							</div>
+							<div className="col-md-4"> </div>
 						</div>
-						<div className="col-md-4"> </div>
+
+						<div className="row margin-bottom-20">
+							<div className="col-md-4"> </div>
+							<div className="col-md-3">
+								<div className="row">
+									<div className="col-md-6"> </div>
+									<div className="col-md-6">
+										<Anchor tabIndex="3" onClick = {() => {Actions.setRoute('/reset-password')}} className="margin-left-6 primary-blue">forgot password</Anchor>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4"> </div>
+						</div>
+
+
+						<div className="row">
+							<div className="col-md-4"> </div>
+							<div className="col-md-3">
+								<div className="mg-book-form-input">
+									<button tabIndex="4" type="submit" onClick = {() => {authenticateUser(this)}} className="width-265 btn btn-primary">Sign In</button>
+								</div>
+							</div>
+							<div className="col-md-4"> </div>
+						</div>
 					</div>
-				</div>
+				</ValidateGroup>
 			</div>
 		);
 	}
