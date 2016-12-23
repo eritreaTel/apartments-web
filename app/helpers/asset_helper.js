c37597ed054b4e30d009294952abf7e0eeb17f72
Amanuel Yohannes <eritreaTel@gmail.com>
2016-12-23 15:19:25 +0300

    configure right bucket

--- app/helpers/asset_helper.js
+++ app/helpers/asset_helper.js
@@ -2,8 +2,8 @@ const join = require('url-join');
 
 module.exports = {
   assetPath(asset) {
-    const {assetHost, assetPort} = require('../../config/config');
-    if (assetHost) return `${join(...[[assetHost, assetPort].filter(Boolean).join(':'), asset])}`;
+    const {assetHost, assetPort, bucketInfo} = require('../../config/config');
+    if (assetHost) return `${join(...[[assetHost, assetPort].filter(Boolean).join(':'), bucketInfo, asset])}`;
     return `/${asset}`;
   }
 };
