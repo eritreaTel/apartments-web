bf8f4f412d0b69aaf79d46371b2e19d800184f31
Amanuel Yohannes <eritreaTel@gmail.com>
2017-01-11 09:17:44 +0300

    book apartment clicked

--- app/dispatchers/page_dispatcher.js
+++ app/dispatchers/page_dispatcher.js
@@ -71,11 +71,8 @@ module.exports = {
 
     bookApartmentClicked({apartmentId}) {
         this.mergeStoreVal('bookingStage', {activeStage: 'personal'});
-        //From one apartment page, we already have apartment, from guest house page, we fetch the apartment
-        if (this.getStoreVal('apartment') == null) {
-            let apartment = this.getStoreVal('apartments').find(apt => apt.id == apartmentId);
-            this.setStoreVal('apartment', apartment);
-        }
+        let apartment = this.getStoreVal('apartments').find(apt => apt.id == apartmentId);
+        this.setStoreVal('apartment', apartment);
     },
 
     bookBestApartmentClicked({apartmentId}) {
