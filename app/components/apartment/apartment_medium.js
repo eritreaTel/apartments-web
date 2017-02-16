489a980d4fb92c808f5ff84d137ac55a2c23e4c6
Amanuel Yohannes <eritreaTel@gmail.com>
2017-02-16 19:48:05 +0300

    fix bug on best apartment section

--- app/components/apartment/apartment_medium.js
+++ app/components/apartment/apartment_medium.js
@@ -18,7 +18,7 @@ class ApartmentMedium extends React.Component {
 
     render() {
         const {apartment} = this.props;
-
+        let pricingInfo = apartment.pricingInfo;
 
         return (
             <figure className="mg-room">
@@ -26,7 +26,7 @@ class ApartmentMedium extends React.Component {
                 <figcaption>
                     <h2>{apartment.title}</h2>
                     <div className="mg-room-rating"><i className="fa fa-star"></i> {apartment.star_rating}</div>
-                    <div className="mg-room-price">{ApplicationHelper.formatCurrency(apartment.price_per_day)}<sup>.00/Night</sup></div>
+                    <div className="mg-room-price">{ApplicationHelper.formatCurrency(pricingInfo.total_price)}<sup>.00</sup><span className="homepage-total_price">/{pricingInfo.days_cnt} Days</span></div>
                     <p>{apartment.short_description}</p>
                     <Anchor onClick={() => {viewBestApartmentClicked(apartment.id);}} className="btn btn-link">View Details <i className="fa fa-angle-double-right"></i></Anchor>
                     <Anchor onClick={() =>{bookBestApartmentClicked(apartment.id);}} className="btn btn-main">Book</Anchor>
