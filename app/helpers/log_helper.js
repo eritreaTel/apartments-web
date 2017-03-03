6c232579c55d0a311b613579c1f93b13c864a742
Amanuel Yohannes <eritreaTel@gmail.com>
2017-03-03 17:55:54 +0300

    logger

--- app/helpers/log_helper.js
+++ app/helpers/log_helper.js
@@ -1,16 +1,3 @@
-var Logger = require('logdna');
-var options = {
-  hostname: "local.ugandaBooking.com",
-  ip: "127.0.1.1",
-  mac: "2c:f0:ee:2e:0d:66",
-  app: "ugandaBooking"
-};
-
-options.index_meta = true;
-
-var logger = Logger.setupDefaultLogger("9b8168b9d3454816a3d7629c16bb5785", options);
-
-
 const debugEnabled = true; // Make it configurable - send only from production
 
 
@@ -23,7 +10,6 @@ const LogHelper = {
   log(message) {
     if (debugEnabled) {
       this.initDebug();
-      logger.info(message);
     }
   },
 
