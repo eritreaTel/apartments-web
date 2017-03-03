6da0e4e20d8eb26d252bda40dc546a22907159a0
Amanuel Yohannes <eritreaTel@gmail.com>
2017-03-03 16:10:47 +0300

    update

--- app/helpers/log_helper.js
+++ app/helpers/log_helper.js
@@ -1,4 +1,3 @@
-var loggly = require('loggly');
 
 
 const debugEnabled = true; // Make it configurable - send only from production
@@ -6,18 +5,7 @@ const debugEnabled = true; // Make it configurable - send only from production
 
 const LogHelper = {
   initDebug() {
-    var client = loggly.createClient({
-      token: "5c85d379-9dad-4c13-a0e2-43cc078c7c67",
-      subdomain: "ugandabooking",
-      auth: {
-        username: "ugandabooking",
-        password: "Uganda6ook!ng"
-      },
-      tags: ["Production-WebServer"],
-      json:true
-    });
-
-    client.log("This is it. logged from react");
+    
 
   },
 
