5cf40eada9f49e3c0bd8be6a11b0bbef4119431e
Amanuel Yohannes <eritreaTel@gmail.com>
2017-03-03 17:10:55 +0300

    Revert "update"
    This reverts commit 6da0e4e20d8eb26d252bda40dc546a22907159a0.

--- app/helpers/log_helper.js
+++ app/helpers/log_helper.js
@@ -1,3 +1,4 @@
+var loggly = require('loggly');
 
 
 const debugEnabled = true; // Make it configurable - send only from production
@@ -5,7 +6,18 @@ const debugEnabled = true; // Make it configurable - send only from production
 
 const LogHelper = {
   initDebug() {
-    
+    var client = loggly.createClient({
+      token: "5c85d379-9dad-4c13-a0e2-43cc078c7c67",
+      subdomain: "ugandabooking",
+      auth: {
+        username: "ugandabooking",
+        password: "Uganda6ook!ng"
+      },
+      tags: ["Production-WebServer"],
+      json:true
+    });
+
+    client.log("This is it. logged from react");
 
   },
 
