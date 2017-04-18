d675c46888cec13bab34d4bb34f51a4f95d681fe
Amanuel Yohannes <eritreaTel@gmail.com>
2017-04-18 23:18:11 +0300

    changes

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+var exphbs  = require('express-handlebars');
 /*const bodyParser = require('body-parser');
 const fs = require('fs');
 */
 const path = require('path');
 const app = express();
-const pathToPublic = path.join(__dirname, 'public');
+const pathToPublic = path.join(__dirname, 'views');
 
 app.set('port', (process.env.PORT || 8080));
-app.set('view engine', 'html');
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static(pathToPublic));
 
 
 app.use(function(req, res, next) {
-  if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https') && req.originalUrl != '/heartbeat.html') {
+  if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https') && req.originalUrl != '/heartbeat') {
     res.redirect('https://' + req.get('Host') + req.url);
   }
   else
@@ -28,39 +33,87 @@ app.get('*.js', function (req, res, next) {
 });
 
 
-app.use('/', express.static(pathToPublic));
-app.use('/index', express.static(pathToPublic));
-app.use('/guest-houses', express.static(pathToPublic));
-app.use('/error', express.static(pathToPublic));
-app.use('/sign-in', express.static(pathToPublic));
-app.use('/reset-password', express.static(pathToPublic));
-app.use('/about-us', express.static(pathToPublic));
-app.use('/contact-us', express.static(pathToPublic));
-app.use('/apartment/*', express.static(pathToPublic));
-app.use('/combo-apartment/*', express.static(pathToPublic));
-app.use('/blogs', express.static(pathToPublic));
-app.use('/blog/*', express.static(pathToPublic));
-app.use('/your-trip', express.static(pathToPublic));
-app.use('/your-trip/*', express.static(pathToPublic));
-app.use('/my-account', express.static(pathToPublic));
-app.use('/reset-password', express.static(pathToPublic));
-app.use('/terms-of-use', express.static(pathToPublic));
-app.use('/privacy-policy', express.static(pathToPublic));
-
-/*app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-*/
-
-
-// universal routing and rendering
-app.get('/', (req, res) => {
-  if (req.originalUrl == '/heartbeat.html') {
-    return res.render('heartbeat');
-  } else {
-    return res.render('index');
-
-  }
+app.get('/heartbeat', (req, res) => {
+  return res.send('<html><head></head><body>success</body></html>');
 });
+
+app.get('/', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/index', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/guest-houses', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/sign-in', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/reset-password', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/about-us', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/contact-us', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/apartment/*', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/combo-apartment/*', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/blogs', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/blog/*', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/your-trip', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/your-trip/*', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/my-account', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/reset-password', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/terms-of-use', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/privacy-policy', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/error', (req, res) => {
+  return res.render('home');
+});
+
+app.get('/', (req, res) => {
+    return res.render('home');
+});
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
