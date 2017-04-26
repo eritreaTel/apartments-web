287dfb188249d5b3c363df359146c8cd39154717
Amanuel Yohannes <eritreaTel@gmail.com>
2017-04-26 16:09:20 +0300

    local webpack configuration

--- config/local_webpack.config.js
+++ config/local_webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
       pathinfo: true
     },
     devServer: {
-        contentBase: "./public",
+        contentBase: "./view",
         colors: true,
         historyApiFallback: true,
         inline: true
