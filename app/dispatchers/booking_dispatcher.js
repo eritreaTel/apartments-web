39fb923b237a6187cc10bb2bee66bcb7c7e5785e
Amanuel Yohannes <eritreaTel@gmail.com>
2017-06-08 17:09:33 +0300

    cleanup booking dispatcher

--- app/dispatchers/booking_dispatcher.js
+++ app/dispatchers/booking_dispatcher.js
@@ -19,7 +19,8 @@ module.exports = {
     },
 
     async createApartmentBooking({stripe_token}) {
-        const url = "apartment_bookings?XDEBUG_SESSION_START='PHPSTORM'";
+        const url = "apartment_bookings";
+        //const url = "apartment_bookings?XDEBUG_SESSION_START='PHPSTORM'";
         this.setStoreVal('requestUrl', url);
 
         if (this.acquireLock('createApartmentBooking')) {
